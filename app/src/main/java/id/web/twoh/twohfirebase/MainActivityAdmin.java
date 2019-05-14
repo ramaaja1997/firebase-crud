b129abeb6ee97c34e521811e09191167b2ce2af2
ramaaja1997 <ramaaja1997@gmail.com>
2019-05-15 02:33:04 +0700

    ui admin

---
+++ app/src/main/java/id/web/twoh/twohfirebase/MainActivityAdmin.java
@@ -0,0 +1,13 @@
+package id.web.twoh.twohfirebase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_admin);
+    }
+}
